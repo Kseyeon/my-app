82cce3b27739700acaa48002bca355201ea65c4b
Kseyeon <verkofa@naver.com>
2020-10-14 20:30:42 +0900

    Updates

---
+++ precache-manifest.b3e4763959562e9fc9777d174b471e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f371a84a9cb2e4bf8385622d9149a8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d395e9235b4da64a8b32",
+    "url": "/my-app/static/css/main.aebc20c0.chunk.css"
+  },
+  {
+    "revision": "d93fe400992f7dc65d30",
+    "url": "/my-app/static/js/2.2cb873af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2cb873af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d395e9235b4da64a8b32",
+    "url": "/my-app/static/js/main.c98126f0.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
